9c5358496cc1e6700f670ce351b1ea39e0efc187
kaveri-g <kaveri.govindasamy@camunda.com>
2025-05-25 19:26:26 +0800

    bucket name added

--- terraform/config.tf
+++ terraform/config.tf
@@ -4,6 +4,9 @@ terraform {
   # You can override the backend configuration; this is  given as an example.
   backend "s3" {
     encrypt = true
+    bucket = "kaveri-lab"
+    key    = "os-eks-terraform/terraform.tfstate"
+    region = "ap-southeast-1"
   }
 
   required_providers {
